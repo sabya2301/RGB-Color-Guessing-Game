902fac895dc7ee9945e888a4dbcae57cf3f97edb
sabya2301 <36478808+sabya2301@users.noreply.github.com>
2020-05-04 14:32:52 +0530

    Add files via upload

--- color.js
+++ color.js
@@ -11,33 +11,58 @@ var mesage = document.getElementById("messageDisplay");
 var h1 = document.querySelector("h1");
 var easy = document.getElementById("easy");
 var hard = document.getElementById("hard");
-
+var sc=0;
+var turns = 3;
+var score = document.getElementById("score");
+var canPlay = true;
+var lost = document.getElementById("lost");
+var l =0;
 
 for (var i=0; i<colors.length; i++)
 {
-	squares[i].style.backgroundColor = colors[i];
+	squares[i].style.backgroundColor = colors[i];	
 	squares[i].addEventListener("click", function()
-	{
-		pickedColor = this.style.backgroundColor;
-		if(pickedColor === random)
 		{
-			// alert("You Won");
-			mesage.textContent = "You Won !";
-			h1.style.backgroundColor = random;
-			changeColor();
-			reset.textContent = "Play Again";
-		}
-		else
-		{
-			this.style.backgroundColor = "#232323";
-			mesage.textContent = "Try Again";
-		}
-	});
+			if(canPlay)
+			{
+				pickedColor = this.style.backgroundColor;
+				if(pickedColor === random)
+				{
+					// alert("You Won");
+					mesage.textContent = "You Won !";
+					canPlay = false;
+					h1.style.backgroundColor = random;
+					sc++;
+					score.textContent = "Score: " + sc;
+
+					changeColor();
+					reset.textContent = "Play Again";
+					
+				}
+				else
+				{
+					this.style.backgroundColor = "#232323";
+					turns--;
+					mesage.textContent = "Try Again, you have " + turns + " left";
+					if (turns==0){
+						canPlay = false;
+						l++;
+						lost.textContent = "Lost: " + l; 
+					}
+				}
+			}
+		});
 }
 
 var reset = document.getElementById("reset");
 reset.addEventListener("click", function()
 {
+	if(numSquares==6)
+		turns = 3;
+	if(numSquares==3)
+		turns = 1;
+
+	canPlay = true;
 	reset.textContent = "New Colors";
 	h1.style.backgroundColor = "#232323";
 	// alert("C");
@@ -83,6 +108,8 @@ function genRandomColor(n)
 
 easy.addEventListener("click", function()
 {
+	canPlay = true;
+	turns=2;
 	mesage.textContent = "BEST OF LUCK !";
 	numSquares = 3;
 	for(var i=3; i<colors.length; i++)
@@ -104,6 +131,8 @@ easy.addEventListener("click", function()
 
 hard.addEventListener("click", function()
 {
+	canPlay = true;
+	turns = 3;
 	mesage.textContent = "BEST OF LUCK !";
 	numSquares = 6;
 	this.classList.add("selected");
@@ -122,4 +151,10 @@ hard.addEventListener("click", function()
 		squares[i].style.display = "block";
 	}
 
+})
+
+var help = document.getElementById("help");
+help.addEventListener("click", function()
+{
+	alert("Guess the color using the pallete of RGB() where R stands for Red, G for Green and B for Blue. The colors are on a scale of 0-255.You need to guess the color which can be formed by combining the given value of the three colors.");
 })
